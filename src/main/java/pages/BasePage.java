de504cf85b19763df998fe3fe234ed10b9710ab4
EKiselev <mr.kiseleff@list.ru>
2020-02-04 16:34:56 +0300

    Selenium YandexTest Allure + cucumber ready, but maven is not run

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -10,9 +10,7 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.util.concurrent.TimeUnit;
-
 
 import static org.junit.Assert.assertEquals;
 import static util.Init.getDriver;
